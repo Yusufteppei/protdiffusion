5739ca4058b998d6e5dec748c1edbbf60c7068ee
Yusuf <yusufteppei11@gmail.com>
2026-08-14 09:27:25 +0100

    fix cuda in config

--- rfdiffusion/config.py
+++ rfdiffusion/config.py
@@ -1,4 +1,4 @@
-import cuda
+import torch
 
 n_res = NUM_RESIDUES = 22
 d_res = RESIDUE_DIM = 256
@@ -6,4 +6,4 @@ d_pair = PAIR_DIM = 128
 
 n_h = NUM_HEADS = 4
 
-device = "cuda" if cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
