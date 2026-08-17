2be81315eb925843669115cbdfb4f44bfea1e4c7
Yusuf <yusufteppei11@gmail.com>
2026-08-17 19:46:42 +0100

    diffusion forward

--- rfdiffusion/diffusion/diffuser.py
+++ rfdiffusion/diffusion/diffuser.py
@@ -1,16 +1,23 @@
 import torch
 import torch.nn as nn
-from rfdiffusion.diffusion import TranslationDiffusion, RotationDiffuser
+from rfdiffusion.diffusion import TranslationDiffuser, RotationDiffuser
+from rfdiffusion.geometry import Rigid
 
 
 class Diffuser(nn.Module):
-    def __init__(self, timesteps=200):
+    def __init__(self, num_timesteps=200):
         super().__init__()
 
-        self.translation = TranslationDiffusion(timesteps=timesteps)
-        self.rotation = RotationDiffuser(timesteps=timesteps)
-        #self.T = torch.randint(timesteps, (B, ))
+        self.num_timesteps = num_timesteps
+        self.translation = TranslationDiffuser(num_timesteps=num_timesteps)
+        self.rotation = RotationDiffuser(num_timesteps=num_timesteps)
 
-    def forward(self, t):
+    def forward(self, x0: Rigid, timestep):
+        x0_r, x0_t = x0.rotation, x0.translation
+        
+        xt_tr, noise_tr = self.translation(x0_t, timestep)
+        xt_r, noise_r = self.rotation(x0_r, timestep)
 
-        trans = self.translation(x0, t, noise)
+        xt, noise_t = Rigid(rotation=xt_r, translation=xt_tr), Rigid(rotation=noise_r, translation=noise_tr)
+
+        return xt, noise_t
--- rfdiffusion/diffusion/rotation_diffuser.py
+++ rfdiffusion/diffusion/rotation_diffuser.py
@@ -4,9 +4,12 @@ import torch.nn as nn
 
 class RotationDiffuser(nn.Module):
 
-    def __init__(self, timesteps):
+    def __init__(self, num_timesteps):
         super().__init__()
 
+        self.num_timesteps = num_timesteps        
 
-    def forward(self):
-        pass
+
+    def forward(self, x0, timestep, noise=None):
+
+        return x0, noise
